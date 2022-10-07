2f7308e9d8e6302ecfe3e7761e0cb5564dc20315
Nick <varivonnick@gmail.com>
2022-10-07 08:21:51 +0300

    my first RESTAPI

---
+++ api_lesson/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from api_lesson.models import Product
+
+# Register your models here.
+admin.site.register(Product)
---
+++ api_lesson/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApiLessonConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'api_lesson'
